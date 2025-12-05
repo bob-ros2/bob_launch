00be7255f1ff7e183d121be71732e37244a2ca09
Bob Ros <bob-ros@t-online.de>
2025-12-05 17:04:32 +0100

    cleanup

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,7 +2,7 @@
 
 CONF=$1
 NODESCONF=$2
-LARGS=--debug
+
 
 usage_exit() { # [<msg> [<rc>]]
     [ -z "$1" ] || echo "$1"
@@ -25,7 +25,7 @@ export BOB_LAUNCH_CONFIG=$CONF
 if [ -t 0 ]; then
     [ -n "$BOB_LAUNCH_CONFIG" ] || usage_exit "Missing Input!" 1
     [ -z "$NODESCONF" ] || NODESCONF="config_nodes:=$NODESCONF"
-    ros2 launch bob_launch generic.launch.py $NODESCONF $LARGS
+    ros2 launch bob_launch generic.launch.py $NODESCONF --debug
 else
     temp=$(mktemp --suffix .yaml)
     trap "rm -f $temp 2>/dev/null" EXIT
@@ -34,5 +34,5 @@ else
     done
     export BOB_LAUNCH_CONFIG=$temp
     [ -z "$CONF" ] || CONF="config_nodes:=$CONF"
-    ros2 launch bob_launch generic.launch.py $CONF $LARGS
+    ros2 launch bob_launch generic.launch.py $CONF --debug
 fi
